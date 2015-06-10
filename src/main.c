f0c68c2c69fa937e885d03eba501018227db33fd
Ikey Doherty <ikey.doherty@gmail.com>
2015-06-10 19:54:20 +0100

    Add initial skeleton

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c - This file is part of columbiad
+ *
+ * Copyright (C) 2015 Ikey Doherty <ikey.doherty@gmail.com>
+ *
+ * fullscreen-tv-launcher is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <stdlib.h>
+
+#include <gtk/gtk.h>
+
+int main(int argc, char **argv)
+{
+        gtk_init(&argc, &argv);
+
+        return EXIT_SUCCESS;
+}
