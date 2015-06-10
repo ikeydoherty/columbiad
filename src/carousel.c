a72a590703e5e0682d8242a585895d76bac6cedd
Ikey Doherty <ikey.doherty@gmail.com>
2015-06-10 21:41:41 +0100

    Add AppCarousel boilerplate

---
+++ src/carousel.c
@@ -0,0 +1,47 @@
+/*
+ * carousel.c
+ * 
+ * Copyright 2015 Ikey Doherty <ikey.doherty@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include "carousel.h"
+
+G_DEFINE_TYPE(AppCarousel, app_carousel, GTK_TYPE_BIN)
+
+/* Boilerplate GObject code */
+static void app_carousel_class_init(AppCarouselClass *klass);
+static void app_carousel_init(AppCarousel *self);
+static void app_carousel_dispose(GObject *object);
+
+/* Initialisation */
+static void app_carousel_class_init(AppCarouselClass *klass)
+{
+        GObjectClass *g_object_class;
+        g_object_class = G_OBJECT_CLASS(klass);
+
+        g_object_class->dispose = &app_carousel_dispose;
+}
+
+static void app_carousel_init(__attribute__ ((unused)) AppCarousel *self)
+{
+}
+
+
+static void app_carousel_dispose(GObject *object)
+{
+        G_OBJECT_CLASS (app_carousel_parent_class)->dispose (object);
+}
+
+/* Utility; return a new AppCarousel */
+AppCarousel *app_carousel_new()
+{
+        AppCarousel *self;
+
+        self = g_object_new(APP_CAROUSEL_TYPE, NULL);
+        return self;
+}
---
+++ src/carousel.h
@@ -0,0 +1,46 @@
+/*
+ * carousel.h
+ * 
+ * Copyright 2015 Ikey Doherty <ikey.doherty@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ */
+
+#pragma once
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+typedef struct _AppCarousel AppCarousel;
+typedef struct _AppCarouselClass   AppCarouselClass;
+
+#define APP_CAROUSEL_TYPE (app_carousel_get_type())
+#define APP_CAROUSEL(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), APP_CAROUSEL_TYPE, AppCarousel))
+#define IS_APP_CAROUSEL(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), APP_CAROUSEL_TYPE))
+#define APP_CAROUSEL_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), APP_CAROUSEL_TYPE, AppCarouselClass))
+#define IS_APP_CAROUSEL_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), APP_CAROUSEL_TYPE))
+#define APP_CAROUSEL_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), APP_CAROUSEL_TYPE, AppCarouselClass))
+
+/* AppCarousel object */
+struct _AppCarousel {
+        GtkBin parent;
+};
+
+/* AppCarousel class definition */
+struct _AppCarouselClass {
+        GtkBinClass parent_class;
+};
+
+GType app_carousel_get_type(void);
+
+/**
+ * Construct a new AppCarousel
+ *
+ * @param app Parent application instance
+ * @return A new AppCarousel
+ */
+AppCarousel *app_carousel_new(void);
